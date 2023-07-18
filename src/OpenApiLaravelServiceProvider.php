98351e11f631d2f6cbc072c4e264884b225962a1
Chinmay Purav <chinpurav@gmail.com>
2023-07-19 00:01:39 +0530

    initial commit

---
+++ src/OpenApiLaravelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Chinmay\OpenApiLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class OpenApiLaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
