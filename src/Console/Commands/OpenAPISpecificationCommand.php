d3d160f7d040dc1440123b6c1df234801dbf3a03
Chinmay Purav <chinpurav@gmail.com>
2024-03-06 01:46:10 +0530

    OpenAPISpecificationCommand basic logic added

--- src/Console/Commands/OpenAPISpecificationCommand.php
+++ src/Console/Commands/OpenAPISpecificationCommand.php
@@ -2,7 +2,9 @@
 
 namespace Chinmay\OpenApiLaravel\Console\Commands;
 
+use Chinmay\OpenApiLaravel\Factory;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class OpenAPISpecificationCommand extends Command
 {
@@ -10,8 +12,10 @@ class OpenAPISpecificationCommand extends Command
 
     protected $description = 'Generated Open API Specification';
 
-    public function handle():void
+    public function handle(Factory $factory):void
     {
+        $json = $factory->make()->toJson(JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 
+        File::put(storage_path('open-api.json'), $json);
     }
 }
